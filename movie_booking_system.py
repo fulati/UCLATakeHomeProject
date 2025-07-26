5348f475106ec0587325983b3995a5d64171fe23
fulati <faizihaer@gmail.com>
2025-07-26 12:12:24 -0700

    Initial Structure of the program

---
+++ movie_booking_system.py
@@ -0,0 +1,35 @@
+#Imports
+
+
+#Movie Class (title, genre, duration, rating)
+
+
+#Theater Class (name, location)
+
+
+#Screen Class (screen_id, theater, rows, seats_per_row)
+
+
+#Show Class (show_id, movie, screen, time)
+
+
+#Booking Class (user, show, seats)
+
+
+#MovieBookingSystem Class with (movies, theaters, shows, bookings)
+
+    #search_movies function
+
+
+    #list_showtime function
+
+
+    #book_seats function
+
+
+    #cancel_bookings function
+
+
+
+#Main
+
