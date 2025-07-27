39d95ea654fd600ac33a94bfc0754d3149edf90d
fulati <faizihaer@gmail.com>
2025-07-27 09:50:13 -0700

    Added unit test for booking and double booking

--- movie_booking_unit_test.py
+++ movie_booking_unit_test.py
@@ -21,7 +21,18 @@ class TestMovieBookingSystem(unittest.TestCase):
         results = self.system.search_movies("incepTion")
         self.assertTrue(self.movie in results)
         
+    def test_seat_booking_success(self):
+        result = self.system.book_seats("show1", ["A1", "A2"], user="fulati")
+        self.assertTrue(result)
+        # Check if A1 and A2 are booked
+        self.assertIn("A1", self.show.booked_seats)
+        self.assertIn("A2", self.show.booked_seats)
     
+    def test_seat_booking_double_booking(self):
+        self.system.book_seats("show1", ["A1"], user="fulati")
+        # Book A1 again
+        result = self.system.book_seats("show1", ["A1"], user="guest")
+        self.assertFalse(result)
     
 if __name__ == '__main__': 
     unittest.main()
